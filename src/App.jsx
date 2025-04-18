3c277f18b6560e211cf7784ac2da88e228c4a3eb
meanzzi <meanzi.kim@gmail.com>
2025-04-18 23:15:49 +0900

    라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
+// / : 모든 일기 조회 home 페이지
+// /new : 새로운 일기 작성 페이지
+// /diary : 일기 조회 페이지
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
