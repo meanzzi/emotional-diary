58ed95b2dba811ed200d74efd1706b3cc7a63ff8
meanzzi <meanzi.kim@gmail.com>
2025-04-20 23:26:47 +0900

    폰트 변경, 이미지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
+
 // / : 모든 일기 조회 home 페이지
 // /new : 새로운 일기 작성 페이지
 // /diary : 일기 조회 페이지
@@ -17,6 +23,13 @@ function App() {
 
   return (
     <>
+      <div>
+        <img src={emotion1} />
+        <img src={emotion2} />
+        <img src={emotion3} />
+        <img src={emotion4} />
+        <img src={emotion5} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
